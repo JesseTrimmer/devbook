f54aac3e9b275133a6d544edd1f8cc2088136fdd
MaeTrimmer <jessicamtrimmer@gmail.com>
2026-05-22 21:34:27 -0400

    Fix CORS for production deployment

--- server/server.js
+++ server/server.js
@@ -7,17 +7,26 @@ const authRoutes = require('./routes/authRoutes');
 const serviceRoutes = require('./routes/serviceRoutes');
 const bookingRoutes = require('./routes/bookingRoutes');
 const estimateRoutes = require('./routes/estimateRoutes');
+
 const app = express();
 
 connectDB();
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173' }));
+app.use(cors({
+  origin: [
+    process.env.CLIENT_URL,
+    'http://localhost:5173',
+    'http://localhost:3000'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/services', serviceRoutes);
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/estimate', estimateRoutes);
+
 app.get('/', (req, res) => res.json({ message: 'DevBook API running' }));
 
 const PORT = process.env.PORT || 5000;
